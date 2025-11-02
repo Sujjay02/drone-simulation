2cac18228e63252fe9f389c5c5cdac5e63e633cf
Sujjay02 <sujjaykarthikeyan@gmail.com>
2025-11-01 23:14:30 -0400

    added auto takeoff

--- takeoff.sh
+++ takeoff.sh
@@ -1,9 +1,15 @@
 echo "arming"
-
-ros2 service call /$UAV_NAME/hw_api/arming std_srvs/srv/SetBool '{"data": true}' 
+ros2 service call /$UAV_NAME/hw_api/arming std_srvs/srv/SetBool '{"data": true}'
 
 sleep 1.0
 
 echo "toggling offboard"
+ros2 service call /$UAV_NAME/offboard std_srvs/srv/Trigger '{}'
 
-ros2 service call /$UAV_NAME/hw_api/offboard std_srvs/srv/Trigger '{}' 
+sleep 1.0
+
+# ----------------------------------------------------
+# *** NEW: The actual takeoff command- added service calls***
+# ----------------------------------------------------
+echo "commanding takeoff to 1.5m"
+ros2 service call /$UAV_NAME/control_manager/takeoff mrs_msgs/srv/Vec4 "{goal: {x: 0.0, y: 0.0, z: 1.5, yaw: 0.0}}"
